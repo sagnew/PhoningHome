305961f2632b599b7b371e1f53778876b2ff5da9
Sam Agnew <sagnew@eden.rutgers.edu>
2016-04-28 16:17:43 -0400

    Add error checking

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from twilio import twiml
 import requests
 
 rover_url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
+default_image = 'http://mars.jpl.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/ncam/NLB_486264973EDR_S0481570NCAM00546M_.JPG'
 
 app = Flask(__name__)
 
@@ -15,12 +16,16 @@ def get_mars_img():
     sol = request.form['Body']
     request_params = {'api_key': 'DEMO_KEY', 'sol': sol}
 
-    mars_data = requests.get(rover_url, params=request_params).json()
-    photos = mars_data['photos']
-    random_photo_url = choice(photos)['img_src']
+    try:
+        mars_data = requests.get(rover_url, params=request_params).json()
+        photos = mars_data['photos']
+        random_photo_url = choice(photos)['img_src']
 
-    with response.message("Here's a picture from the Mars Rover :)") as m:
-        m.media(random_photo_url)
+        with response.message("Here's a picture from the Mars Rover :)") as m:
+            m.media(random_photo_url)
+    except:
+        with response.message("404 Mars not found. Here's one anyway :)") as m:
+            m.media(default_image)
 
     return str(response)
 

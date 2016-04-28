56899f1e27969bbb7106c9bfa895843007e11087
Sam Agnew <sagnew@eden.rutgers.edu>
2016-04-28 15:14:21 -0400

    Implement basic flask app for retrieving random mars photos

---
+++ app.py
@@ -0,0 +1,22 @@
+from random import choice
+
+from flask import Flask, request, redirect
+import requests
+
+rover_url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
+
+app = Flask(__name__)
+
+
+@app.route('/img', methods=['GET'])
+def get_mars_img():
+    sol = request.args.get('sol')
+    request_params = {'api_key': 'DEMO_KEY', 'sol': sol}
+
+    response = requests.get(rover_url, params=request_params).json()
+    photos = response['photos']
+    random_photo_url = choice(photos)['img_src']
+
+    return redirect(random_photo_url)
+
+app.run(debug=True)

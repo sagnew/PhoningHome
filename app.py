6d8abb97aa6ba494d7ed4a2281d6ec6a89b5eec7
Sam Agnew <sagnew@eden.rutgers.edu>
2016-04-28 16:03:32 -0400

    Respond to text messages with no error checking

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from random import choice
 
-from flask import Flask, request, redirect
+from flask import Flask, request
+from twilio import twiml
 import requests
 
 rover_url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
@@ -8,15 +9,19 @@ rover_url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos'
 app = Flask(__name__)
 
 
-@app.route('/img', methods=['GET'])
+@app.route('/sms', methods=['GET', 'POST'])
 def get_mars_img():
-    sol = request.args.get('sol')
+    response = twiml.Response()
+    sol = request.form['Body']
     request_params = {'api_key': 'DEMO_KEY', 'sol': sol}
 
-    response = requests.get(rover_url, params=request_params).json()
-    photos = response['photos']
+    mars_data = requests.get(rover_url, params=request_params).json()
+    photos = mars_data['photos']
     random_photo_url = choice(photos)['img_src']
 
-    return random_photo_url
+    with response.message("Here's a picture from the Mars Rover :)") as m:
+        m.media(random_photo_url)
+
+    return str(response)
 
 app.run(debug=True)
